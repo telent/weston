ee94212967ebe7ebfaf330b590f21c3ec4f5f3d5
Daniel Barlow <dan@telent.net>
2016-01-22 12:19:06 +0000

    lose bind_key
    instead of calling into C to bind each key, we're going to have a
    "keymap" object in JS

--- src/js.c
+++ src/js.c
@@ -18,16 +18,8 @@ when do we run js functions?
 
 #include "duktape.h"
 
-static int bind_key_event(duk_context *ctx) {
-	int i;
-	// int n = duk_get_top(ctx);  /* #args */
 #include "js.h"
 
-        const char *keysym = duk_safe_to_string(ctx, 0);
-        const char *defn = duk_safe_to_string(ctx, 1);
-        fprintf(stderr, "bound %s to %s\n", keysym, defn);
-	duk_push_number(ctx, 42);
-	return 1;  /* one return value */
 static duk_context *ctx;
 
 }
@@ -37,10 +29,10 @@ void duk_init(void) {
         ctx = duk_create_heap_default();
         duk_eval_string(ctx, "print('Hello world!');");
         
-        duk_push_global_object(ctx);
-        duk_push_c_function(ctx, bind_key_event, DUK_VARARGS);
-        duk_put_prop_string(ctx, -2, "bind_key");
-        duk_pop(ctx);  /* pop global */
+        /* duk_push_global_object(ctx); */
+        /* duk_push_c_function(ctx, bind_key_event, DUK_VARARGS); */
+        /* duk_put_prop_string(ctx, -2, "bind_key"); */
+        /* duk_pop(ctx);  /\* pop global *\/ */
         
         if (duk_peval_file(ctx, "weston-init.js") != 0) {
                 printf("Error: %s\n", duk_safe_to_string(ctx, -1));
