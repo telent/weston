1bbdd50e93e162e22fd72726c8f207a7b16471f7
Daniel Barlow <dan@telent.net>
2016-01-22 14:16:06 +0000

    prototype for js_run_key_binding

--- src/js.h
+++ src/js.h
@@ -1,1 +1,5 @@
 void duk_init(void);
+int js_run_key_binding(struct weston_keyboard *keyboard,
+                       uint32_t time,
+                       uint32_t key,
+                       enum weston_keyboard_modifier modifier);
